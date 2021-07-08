7f6940c0b3a2cb205bc727b8fd1e0105ec95aaee
Rodrigo Costa <rodscosta@gmail.com>
2021-07-08 17:01:43 +0100

    start the functionality of Earnings front end

--- src/pages/Earnings/Earnings.js
+++ src/pages/Earnings/Earnings.js
@@ -1,5 +1,71 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 const Earnings = () => {
-  return <div>Earnings</div>;
+  const [totalEarnings, setTotalEarnings] = useState(0);
+  const [lastMonthEarnings, setLastMonthEarnings] = useState(0);
+  const [earningsByDate, setEarningsBydate] = useState([]);
+
+  React.useEffect(() => {
+    fetchTotalEarnings();
+    fetchLastMonthEarnings();
+    fetchEarningsByDate();
+  }, []);
+
+  function fetchTotalEarnings() {
+    axios
+      .get("/earnings")
+      .then(function (response) {
+        setTotalEarnings(response.data.total_earnings);
+      })
+
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  function fetchLastMonthEarnings() {
+    axios
+      .get("/earnings/last-month")
+      .then(function (response) {
+        setLastMonthEarnings(response.data.total_earnings);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  function fetchEarningsByDate() {
+    axios
+      .get("/earnings/earnings-by-date")
+      .then(function (response) {
+        setEarningsBydate(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div>
+      <h1>Total Earnings</h1>
+      <p>{totalEarnings}</p>
+      <h1>Month Earnings</h1>
+      <p>{lastMonthEarnings}</p>
+      <h1>Earnings By Date</h1>
+      <div>
+        {earningsByDate.map(function (earn, index) {
+          return (
+            <div>
+              <p>{earn.name}</p>
+              <p>{earn.appointment_date}</p>
+              <p>{earn.treatments_earnings}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Earnings;
